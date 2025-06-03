f4b222943c4713ab4d7d1c30d96b7c4c1753d768
PauloMats <pm.mats98@gmail.com>
2025-06-03 16:13:54 -0300

    chore(desktop4): update desktop4 to latest use component Card

--- src/components/desktop4/desktop4.tsx
+++ src/components/desktop4/desktop4.tsx
@@ -1,20 +1,20 @@
 import { Container, GradientOverlay } from './style'
+import Card from '../card/card'
 
 export default function Desktop4() {
   return (
     <Container>
       <GradientOverlay />
-      <div className="relative z-10 flex flex-col justify-center items-start text-white h-full px-12 max-w-[600px]">
-        <h2 className="text-4xl font-bold text-white">
-          CRÉDITO <span className="text-yellow-500">PJ</span>
-        </h2>
-        <ul className="mt-4 space-y-1 text-sm text-white font-light">
-          <li><strong>Oportunidades comerciais</strong></li>
-          <li>Suporte confiável e personalizado</li>
-          <li>Apoio para seu negócio</li>
-          <li>Alcance novos horizontes</li>
-          <li>Linha de crédito para startups</li>
-        </ul>
+      <div className="relative z-10 flex flex-col justify-center items-start text-white h-full px-14 max-w-[600px]">
+        <Card
+              title="Crédito <span>PJ</span>"
+              text="Faça e Receba pagamentos de BDM Digital <br>
+Aumente a visibilidade do seu negócio para milhares de usuários com o <b>BDMap</b>. <br>
+Gestão de funcionários <br>
+Controle de lucros e despesas <br>
+Diversos Benefícios em uma única plataforma."
+              gap={4}
+            />
         <button className="mt-6 bg-yellow-500 text-black font-semibold px-6 py-2 rounded hover:bg-yellow-600 transition">
           Saiba Mais
         </button>
