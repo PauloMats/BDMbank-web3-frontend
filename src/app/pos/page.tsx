56ecd7c6762ed74689218ce82586f34ef29f249f
PauloMats <pm.mats98@gmail.com>
2025-06-11 15:25:40 -0300

    chore(pos-page): adjust page for use reusable component

--- src/app/pos/page.tsx
+++ src/app/pos/page.tsx
@@ -1,30 +1,14 @@
-"use client";
 import React from 'react';
+import ProductHero from '@/components/ProductHero/ProductHero';
 
 const PosPage = () => {
   return (
-    <main className="relative min-h-screen bg-black">
-      {/* Imagem de Fundo 
-          IMPORTANTE: Troque '/img/background-pos.jpg' pelo caminho real da sua imagem.
-      */}
-      <div 
-        className="absolute top-0 left-0 w-full h-full bg-cover bg-center opacity-40"
-        style={{ backgroundImage: "url('/img/background-pos.jpg')" }}
-      ></div>
-
-      {/* Container para o conteúdo sobre a imagem */}
-      <div className="relative flex items-center min-h-screen">
-        <div className="container mx-auto px-6 lg:px-20 text-white">
-            <div className="max-w-2xl pt-24">
-                <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
-                    Vantagens para quem quer vender mais
-                </h1>
-                <p className="text-lg md:text-xl">
-                    Descubra a excelência em transações financeiras com as Maquininhas de Cartão de Crédito da BDM. Projetadas para atender a todo tipo de demandas comerciais. Desde pequenos estabelecimentos até grandes empresas, proporciona conveniência e eficiência, garantindo uma experiência de pagamento perfeita para os clientes.
-                </p>
-            </div>
-        </div>
-      </div>
+    <main>
+      <ProductHero 
+        title="Vantagens para quem quer vender mais"
+        description="Descubra a excelência em transações financeiras com as Maquininhas de Cartão de Crédito da BDM. Projetadas para atender a todo tipo de demandas comerciais. Desde pequenos estabelecimentos até grandes empresas, proporciona conveniência e eficiência, garantindo uma experiência de pagamento perfeita para os clientes."
+        backgroundImageUrl="/img/background-pos.jpg" // Lembre-se de usar o caminho correto para sua imagem
+      />
     </main>
   );
 };
