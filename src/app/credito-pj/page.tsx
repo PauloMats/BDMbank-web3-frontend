01c5855c5e52b1d868dbdb8a5f0f8fb4bec99e3e
PauloMats <pm.mats98@gmail.com>
2025-06-11 15:26:45 -0300

    feat(credito-pj-page): add basic structure for credit page

---
+++ src/app/credito-pj/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProductHero from '@/components/ProductHero/ProductHero';
+
+const CreditoPJPage = () => {
+  return (
+    <main>
+      <ProductHero 
+        title="Crédito PJ para Impulsionar sua Empresa"
+        description="Linhas de crédito com condições especiais para pessoas jurídicas. Invista no crescimento da sua empresa com o apoio financeiro que você precisa, de forma rápida e sem burocracia."
+        backgroundImageUrl="/img/background-credito-pj.jpg" // Altere para a imagem correspondente
+      />
+    </main>
+  );
+};
+
+export default CreditoPJPage;
