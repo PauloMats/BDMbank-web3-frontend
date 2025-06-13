925a01056435308bf403c1e375a16f142ee7c2b1
PauloMats <pm.mats98@gmail.com>
2025-06-13 12:39:37 -0300

    chore(Component ProductHero): Adjustment for better alignment

--- src/components/ProductHero/ProductHero.tsx
+++ src/components/ProductHero/ProductHero.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// Define a interface para as props do componente, garantindo a tipagem
 interface ProductHeroProps {
   title: string;
   description: string;
@@ -12,18 +11,19 @@ const ProductHero: React.FC<ProductHeroProps> = ({ title, description, backgroun
     <div className="relative min-h-screen bg-black">
       {/* Imagem de Fundo */}
       <div 
-        className="absolute top-0 left-0 w-full h-full bg-cover bg-center opacity-40"
+        className="absolute top-0 left-0 w-full h-full bg-cover bg-end opacity-40"
         style={{ backgroundImage: `url(${backgroundImageUrl})` }}
       ></div>
 
       {/* Container do Conteúdo */}
-      <div className="relative flex items-center min-h-screen">
-        <div className="container mx-auto px-6 lg:px-20 text-white">
-            <div className="max-w-2xl pt-24">
+      <div className="relative flex items-end min-h-screen">
+        <div className="container mx-auto px-6 lg:px-10 pb-20 text-white">
+            <div className="pt-24">
                 <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
                     {title}
                 </h1>
-                <p className="text-lg md:text-xl">
+            
+                <p className="text-base md:text-lg text-gray-400">
                     {description}
                 </p>
             </div>
